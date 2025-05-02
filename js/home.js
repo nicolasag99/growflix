dc0c200a99d07ce34e6f6ede0b73ddbebaf865d8
Nicolas Gonçalves <106269836+nicolasag99@users.noreply.github.com>
2025-05-02 12:55:29 -0300

    Upload project

---
+++ js/home.js
@@ -0,0 +1,275 @@
+const movies = [
+{
+    img: "https://img.youtube.com/vi/aJ-HZuLcKeA/sddefault.jpg",
+    title: "Growcast #00 - Conheça a Growdev",
+    link: "aJ-HZuLcKeA",
+    category: "Growcast [Episódios]",
+},
+{
+    img: "https://img.youtube.com/vi/7OWT3lfHYvE/sddefault.jpg",
+    title: "Growcast #01 - As profissões do futuro",
+    link: "7OWT3lfHYvE",
+    category: "Growcast [Episódios]",
+},
+{
+    img: "https://img.youtube.com/vi/ci1eQGPK89o/sddefault.jpg",
+    title: "Growcast #02 - Como seguir a carreira internacional?",
+    link: "ci1eQGPK89o",
+    category: "Growcast [Episódios]",
+},
+{
+    img: "https://img.youtube.com/vi/WM7pGG7OicQ/sddefault.jpg",
+    title: "Growcast #03 - Superando os desafios da carreira de Dev!",
+    link: "WM7pGG7OicQ",
+    category: "Growcast [Episódios]",
+},
+{
+    img: "https://img.youtube.com/vi/2c8VOBZ160Y/sddefault.jpg",
+    title: "Growcast #04 - Agile Master: tudo sobre a profissão!",
+    link: "2c8VOBZ160Y",
+    category: "Growcast [Episódios]",
+},
+{
+    img: "https://img.youtube.com/vi/mxnEm03cYPc/sddefault.jpg",
+    title: "Growcast #05 - Como iniciar na carreira em Desenvolvimento de Softwares?",
+    link: "mxnEm03cYPc",
+    category: "Growcast [Episódios]",
+},
+{
+    img: "https://img.youtube.com/vi/m7DyR07KrOE/sddefault.jpg",
+    title: "Growcast #06 - Mulheres e inclusão na tecnologia",
+    link: "m7DyR07KrOE",
+    category: "Growcast [Episódios]",
+},
+{
+    img: "https://img.youtube.com/vi/vW8G9KPwRL0/sddefault.jpg",
+    title: "Growcast #07 - Desenvolvendo profissionais na área de TI",
+    link: "vW8G9KPwRL0",
+    category: "Growcast [Episódios]",
+},
+{
+    img: "https://img.youtube.com/vi/_Ut2KqAqPyk/sddefault.jpg",
+    title: "Growcast #08 - A solução que está transformando o mercado de TI no Brasil",
+    link: "_Ut2KqAqPyk",
+    category: "Growcast [Episódios]",
+},
+{
+    img: "https://img.youtube.com/vi/dfDgs0Ive2Q/sddefault.jpg",
+    title: "Growcast #09 - Acompanhe com quem realmente entende, tudo sobre o mercado de trabalho no mundo da TI",
+    link: "dfDgs0Ive2Q",
+    category: "Growcast [Episódios]",
+},
+
+{
+    img: "https://img.youtube.com/vi/WKbLXuP9S-c/sddefault.jpg",
+    title: "Webinar - Recriando a interface do Linkedin com Flutter",
+    link: "WKbLXuP9S-c",
+    category: "Webinar em Flutter",
+},
+{
+    img: "https://img.youtube.com/vi/dJ6y1zMjVXU/sddefault.jpg",
+    title: "Webinar - Recriando a interface do Instagram com Flutter",
+    link: "dJ6y1zMjVXU",
+    category: "Webinar em Flutter",
+},
+{
+    img: "https://img.youtube.com/vi/8rdJsqUjsGg/sddefault.jpg",
+    title: "Webinar - Recriando a interface do iFood com Flutter",
+    link: "8rdJsqUjsGg",
+    category: "Webinar em Flutter",
+},
+{
+    img: "https://img.youtube.com/vi/C4p_7qBsECw/sddefault.jpg",
+    title: "Webinar - Recriando a interface do PicPay com Flutter",
+    link: "C4p_7qBsECw",
+    category: "Webinar em Flutter",
+},
+{
+    img: "https://img.youtube.com/vi/L1pKoMC6hWk/sddefault.jpg",
+    title: "Webinar - Recriando o layout do WhatsApp com Flutter",
+    link: "L1pKoMC6hWk",
+    category: "Webinar em Flutter",
+},
+{
+    img: "https://img.youtube.com/vi/FiZMowbaiJA/sddefault.jpg",
+    title: "Live #5 Jornada UX/UI: O UX/UI Design é para todos?",
+    link: "FiZMowbaiJA",
+    category: "Jornada UX/UI",
+},
+{
+    img: "https://img.youtube.com/vi/cwqDu0ZzbYE/sddefault.jpg",
+    title: "Live #4 Jornada UX/UI: Como migrar para o UX/UI Design: a trajetória de uma designer - Growdev",
+    link: "cwqDu0ZzbYE",
+    category: "Jornada UX/UI",
+},
+{
+    img: "https://img.youtube.com/vi/ZMZjQ9aRuIY/sddefault.jpg",
+    title: "Live #3 Jornada UX/UI: As oportunidades de estudar um programa de formação pagando após formado",
+    link: "ZMZjQ9aRuIY",
+    category: "Jornada UX/UI",
+},
+{
+    img: "https://img.youtube.com/vi/8sd3wuKTZMA/sddefault.jpg",
+    title: "Live #2 Jornada UX/UI: O profissional de UX/UI Design no mercado digital - Growdev",
+    link: "8sd3wuKTZMA",
+    category: "Jornada UX/UI",
+},
+{
+    img: "https://img.youtube.com/vi/-26aqPtgoEY/sddefault.jpg",
+    title: "Live #1 Jornada UX/UI: O papel do UX/UI Designer no desenvolvimento de um produto digital - Growdev",
+    link: "-26aqPtgoEY",
+    category: "Jornada UX/UI",
+},
+{
+    img: "https://img.youtube.com/vi/QsuhYgsSYQo/sddefault.jpg",
+    title: "Célula de talentos - Uma solução Growdev",
+    link: "QsuhYgsSYQo",
+    category: "Diversos",
+},
+{
+    img: "https://img.youtube.com/vi/ouDn8z3PSqs/sddefault.jpg",
+    title: "Como é o dia a dia de um dev? Live Growdev com Marcelo Eltz",
+    link: "ouDn8z3PSqs",
+    category: "Diversos",
+},
+{
+    img: "https://img.youtube.com/vi/cQDQNX6NXYo/sddefault.jpg",
+    title: "Onde trabalhar com qualidade de software? Conheça a Testing Company, empresa parceira da Growdev",
+    link: "cQDQNX6NXYo",
+    category: "Diversos",
+},
+{
+    img: "https://img.youtube.com/vi/nKGbolkirBM/sddefault.jpg",
+    title: "WEBINAR ANALISTA DE DADOS - com Paulo Marcon da Arezzo&Co",
+    link: "nKGbolkirBM",
+    category: "Diversos",
+},
+{
+    img: "https://img.youtube.com/vi/6sZPsqNjeek/sddefault.jpg",
+    title: "Tecnologia trazendo soluções para a Arezzo & CO | Ricardo Pinho",
+    link: "6sZPsqNjeek",
+    category: "Diversos",
+},
+];
+
+  
+
+
+// Code to dynamically create the video cards
+
+const categorias = {
+    "Growcast [Episódios]": document.getElementById('growcast'),
+    "Webinar em Flutter": document.getElementById('webinar'),
+    "Jornada UX/UI": document.getElementById('jornada'),
+    "Diversos": document.getElementById('diversos')
+  };
+  
+  for (let key in categorias) {
+    if (categorias[key]) {
+      categorias[key].innerHTML = '<div class="row container align-items-center"></div>';
+    }
+  }
+  
+
+  movies.forEach((movie, index) => {
+    const container = categorias[movie.category];
+  
+    if (container) {
+      const row = container.querySelector('.row');
+      const modalId = `myModal-${index}`;   
+      row.innerHTML += `
+        <div class="modal fade" id="${modalId}" tabindex="-1" aria-hidden="true">
+          <div class="modal-dialog modal-fullscreen">
+            <div class="modal-content modal-transparent">
+              <div class="modal-header my-btn-close p-4">
+                <button type="button" id="btnModal" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body p-0 m-0">
+                <div class="video-container">
+                  <iframe width="1920" height="823" src='https://www.youtube.com/embed/${movie.link}' title="${movie.title}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>      
+                </div>
+              </div>
+            </div>
+          </div>  
+        </div>
+        
+        <div class="col-12 col-sm-6 col-md-4 col-xl-3">
+          <div class="content w-100">
+            <img src="${movie.img}" class="w-100 img-content" alt="Capa do video ${movie.title}">
+            <div class="container card-description bg-black ps-3">
+              <div class="row align-items-end">
+                <div class="col m-auto">
+                  <a href="${movie.link}" target="_blank" data-bs-toggle="modal" data-bs-target="#${modalId}" class="text-decoration-none text-light d-flex align-items-center fs-2 mb-3">
+                    <i class="bi bi-play-circle text-light fs-1 m-1 d-flex"></i>
+                    <p class="video-title mb-0 d-flex align-items-center justify-content-start">${movie.title}</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+    } else {
+      console.error(`Categoria não encontrada: ${movie.category}`);
+    } 
+  });
+
+
+// Stop video playback when clicking on btn-close
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('btn-close')) {
+    const modal = event.target.closest('.modal');
+    const iframe = modal.querySelector('iframe');
+    if (iframe) {
+      iframe.src = '';
+    }
+  }
+});
+
+
+
+// Show video description on click (mobile)
+document.querySelectorAll('.img-content').forEach((img) => {
+  img.addEventListener('touchstart', (event) => {
+    const card = img.closest('.content');
+    const description = card.querySelector('.card-description');
+    document.querySelectorAll('.card-description').forEach(desc => {
+      if (desc !== description) {
+        desc.style.display = 'none';
+      }
+    });
+
+    if (description) {
+      description.style.display = 'block';
+    }
+  });
+});
+
+
+// Hide description when tapping outside of it (mobile)
+document.body.addEventListener('touchstart', (event) => {
+  if (!event.target.closest('.card-description') && !event.target.classList.contains('img-content')) {
+    document.querySelectorAll('.card-description').forEach((desc) => {
+      desc.style.display = 'none';
+    });
+  }
+});
+  
+
+
+// Code dropdown menu
+document.getElementById("menuButton").addEventListener("click", (event) => {
+  const menu = document.getElementById("customDropdown");
+  menu.classList.toggle("d-none");
+  event.stopPropagation();
+});
+
+document.addEventListener("click", (event) => {
+  const menu = document.getElementById("customDropdown");
+  const menuButton = document.getElementById("menuButton");
+
+  if (!menu.contains(event.target) && !menuButton.contains(event.target)) {
+    menu.classList.add("d-none");
+  }
+});
+
